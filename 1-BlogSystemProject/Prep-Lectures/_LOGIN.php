1b2fdf40ec0b783cf600254e6b56d738faa910c1
mahmoudRiad22 <mahmoudriad027@gmail.com>
2023-09-24 18:17:27 +0300

    adding new files after finishing lectures 22,23,24,25.

---
+++ 1-BlogSystemProject/Prep-Lectures/_LOGIN.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Cookies</title>
+    </head>
+
+    <body>
+            <?php
+            $info = "last time loggeed in".time();
+            $infoArray = array('name'=>'mahmoud', 'E-mail'=>'mahmoudriad027@gmail.com',
+                            'phone'=>'01023258052', 'lastLog'=>time());
+                setcookie('Lastlogged', $info, time() + (1*60*60),);
+                setcookie('Info', json_encode($infoArray), time()+(1*24*60*60));
+
+                if(isset($_COOKIE['Info']))
+                {
+                    $value_decoded = json_decode($_COOKIE['Info']);
+                    foreach($value_decoded as $element)
+                    {
+                        echo $element . '<br>';
+                    }
+                }
+        ?>
+
+        <h2><?= $_COOKIE['Lastlogged']?></h2>
+        <h2><?= $_COOKIE['Info']?></h2>
+    </body>
+
+    </html>
